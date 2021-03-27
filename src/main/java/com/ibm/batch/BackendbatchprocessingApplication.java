cd61c835e33d0b93087eed0b18f9279d8c85a630
Surajit Mukhopadhyay <ADMSMU216@CRB.APMOLLER.NET>
2021-03-28 00:50:37 +0530

    changed

--- src/main/java/com/ibm/batch/BackendbatchprocessingApplication.java
+++ src/main/java/com/ibm/batch/BackendbatchprocessingApplication.java
@@ -4,8 +4,15 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+
+import com.ibm.redis.listerner.Receiver;
 
 @SpringBootApplication
 @EnableBatchProcessing
@@ -20,5 +27,32 @@ public class BackendbatchprocessingApplication {
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
+	
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	@Bean
+	Receiver receiver() {
+		return new Receiver();
+	}
+
+	@Bean
+	StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
+	}
+	
 
 }
